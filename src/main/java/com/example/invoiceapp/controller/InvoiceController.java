10da20ef10381a5d2c43d295e10042495af22a69
sabeeh shah <sabeeh.shah@hotmail.com>
2021-01-07 00:11:27 -0500

    fix GET mapping URL

--- src/main/java/com/example/invoiceapp/controller/InvoiceController.java
+++ src/main/java/com/example/invoiceapp/controller/InvoiceController.java
@@ -66,7 +66,7 @@ public class InvoiceController {
 		}
 	}
 	
-	@GetMapping("/invoices/{createdBy}")
+	@GetMapping("/invoices/createdBy")
 	public ResponseEntity<List<Invoice>> getInvoicesCreatedBy(@RequestParam(value="createdBy") String createdBy){
 		try {
 			List<Invoice> invoices = new ArrayList<Invoice>();
@@ -78,6 +78,7 @@ public class InvoiceController {
 			}
 			
 			log.info(invoices.toString());
+			log.info("hitting createdBy endpoint");
 			return new ResponseEntity<>(invoices,HttpStatus.OK);
 
 			
